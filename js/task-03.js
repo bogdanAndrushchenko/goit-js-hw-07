1f451e117013772ce1ed731079991bce6190e1be
Bogdan Andrushchenko <bandrushchenko15@gmail.com>
2020-08-21 21:57:09 +0300

    doing task-03.js

--- js/task-03.js
+++ js/task-03.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 /**
  * Напиши скрипт для создания галлереи изображений по массиву данных.
 
@@ -10,29 +10,32 @@
 
 Все элементы галереи должны добавляться в DOM за одну операцию вставки.
 Добавь минимальное оформление галереи флексбоксами или гридами через css-классы.*/
-const images = [{
-        url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        alt: 'White and Black Long Fur Cat',
-    },
-    {
-        url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
-    },
-    {
-        url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-        alt: 'Group of Horses Running',
-    },
+const images = [
+  {
+    url:
+      "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
+  },
+  {
+    url:
+      "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
+  },
 ];
-const imagesRef = document.createElement('img');
-// console.log(imagesRef);
 
-const imagesRefArr = images.map((elem) => {
-    const imagesRef = document.createElement('img');
-    // elem.i.setAttribute(elem.url, '+')
-    // elem.setAttribute(elem.alt, '+')
-    // elem.i.url = elem.url;
-    elem.alt = elem.alt
-    return imagesRef
-})
-// .forEach((img) => img.alt = img.alt)
-console.log(imagesRefArr);
+const createGalleruTag = (arr) => {
+  const imagesRef = document.createElement("img");
+  imagesRef.src = arr.url;
+  imagesRef.alt = arr.alt;
+  imagesRef.classList.add('img-style');
+  return imagesRef;
+};
+
+const readyImgTag = images.map((jpg) => createGalleruTag(jpg));
+const ulGalleryRef = document.querySelector("#gallery");
+ulGalleryRef.append(...readyImgTag);
